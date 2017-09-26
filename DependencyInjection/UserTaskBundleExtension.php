43df1fb78bf991004d90c638df7d6fd11f8e457a
Andrius Sciukas <andrius.sciukas@ubie.io>
2017-09-26 17:20:20 +0300

    Service dependency injection

---
+++ DependencyInjection/UserTaskBundleExtension.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Fintem\UserTaskBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
+/**
+ * Class UserTaskBundleExtension.
+ */
+class UserTaskBundleExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
+    }
+}
